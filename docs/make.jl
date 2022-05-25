c54c855683f354016d10a1f5444d99d3287a3bc8
milocortes <hermilocg@hotmail.com>
2022-05-24 20:31:29 -0500

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-=deploydocs(
+deploydocs(
     repo = "github.com/milocortes/prueba-documenter.git"
-)=
+)
