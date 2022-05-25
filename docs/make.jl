048785c36bf0849bbc81ee33086acf1d7ad2e23d
milocortes <hermilocg@hotmail.com>
2022-05-24 20:26:20 -0500

    Github actions

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using Documenter
+
+makedocs(
+    sitename = "Example",
+    format = Documenter.HTML()
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+=deploydocs(
+    repo = "github.com/milocortes/prueba-documenter.git"
+)=
